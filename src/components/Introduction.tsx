3442c4060cd0c45a2fa2fc851b45393885220a3f
Brett <lungalb@myumanitoba.ca>
2024-12-29 20:05:01 -0600

    break intro into its own component

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Typography, Box, Divider } from '@mui/material'
+import CodeIcon from '@mui/icons-material/Code';
+import { useTheme } from '@mui/material/styles'
+
+const Introduction: React.FC = () => {
+    const theme = useTheme();
+    return (
+        <>
+            <CodeIcon fontSize="large" sx={{ color: `primary.${theme.palette.mode}`, display: 'flex', marginLeft: '7%' }} />
+            <Divider variant="middle" sx={{ borderBottomWidth: 5, width: '50%', marginLeft: '7%' }} />
+            <Typography variant="h2">
+                Hey I'm{' '}
+                <Box
+                    component="span"
+                    sx={{
+                        color: `primary.${theme.palette.mode}`,
+                        fontWeight: 'bold',
+                    }}
+                >
+                    Brett
+                </Box>
+                , A Software Developer
+            </Typography>
+            <Typography variant="subtitle2" sx={{ display: 'flex', marginLeft: '7%', marginTop: '8%' }}>I like to build scalable, performant solutions primarily focusing on backend development. I am a self-starter and leader which has helped me succeed in my current position as a lead developer at QDoc Inc. </Typography>
+        </>
+    )
+}
+
+export default Introduction;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Typography, Grid2, Paper, Box, Divider } from '@mui/material'
-import {useTheme} from '@mui/material/styles'
-import CodeIcon from '@mui/icons-material/Code';
-
-import Portrait from '../components/Portrait'
+import { Grid2 } from '@mui/material';
+import Introduction from '../components/Introduction';
+import Portrait from '../components/Portrait';
 const Home: React.FC = () => {
-  const theme = useTheme();
 
   return (
     <Grid2
@@ -15,25 +12,10 @@ const Home: React.FC = () => {
       alignItems="center"
     >
       <Grid2 size={4} textAlign="center" sx={{marginTop:'10%'}}>
-        <CodeIcon fontSize="large" sx={{color:`primary.${theme.palette.mode}`, display:'flex', marginLeft:'7%'}}/>
-        <Divider variant="middle" sx={{ borderBottomWidth: 5, width:'50%', marginLeft:'7%' }} />
-        <Typography variant="h2">
-          Hey I'm{' '}
-          <Box
-            component="span"
-            sx={{
-              color: `primary.${theme.palette.mode}`,
-              fontWeight: 'bold',
-            }}
-          >
-            Brett
-          </Box>
-            , A Software Developer
-        </Typography>
-        <Typography variant="subtitle2" sx={{display:'flex', marginLeft:'7%', marginTop:'8%'}}>I like to build scalable, performant solutions primarily focusing on backend development. I am a self-starter and leader which has helped me succeed in my current position as a lead developer at QDoc Inc. </Typography>
+        <Introduction />
       </Grid2>
       <Grid2 display="flex" justifyContent="center" alignItems="center" size={4}>
-        <Portrait  />
+        <Portrait />
       </Grid2>
       <Grid2 display="flex" justifyContent="center" alignItems="center" size={4}>
         
