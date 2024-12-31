f343b142be935e81832b0c10554aee8f7ffa6057
Brett <lungalb@myumanitoba.ca>
2024-12-30 21:31:44 -0600

    im pretty tired i think ill go watch house now

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,10 +10,13 @@ const About: React.FC = () => {
             justifyContent="center"
             alignItems="center"
         >
-            <Grid2 size={12} display="flex" justifyContent="center" alignItems="center">
-                <Typography variant="h2">About</Typography>
-                <Divider />
+            <Grid2 size={4} />
+            <Grid2 size={4} justifyContent="center" alignItems="center" textAlign="center">
+            <Typography variant="h2">About</Typography>
+                <Divider variant="middle" />
+                <Typography variant="body1" sx={{ display: 'flex', marginTop: '5%' }}>I like to build scalable, performant solutions primarily focusing on backend development. I am a self-starter and leader which has helped me succeed in my current position as a lead developer at QDoc Inc. </Typography>
             </Grid2>
+            <Grid2 size={4} />
 
         </Grid2>
     );
